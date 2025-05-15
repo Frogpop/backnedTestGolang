f6469836bdcb839a646b1264d7c1e9b4f919cfc7
Frogpop <frogpop.forg@gmail.com>
2025-05-15 20:27:35 +0700

    add cart service

---
+++ internal/services/CartService.go
@@ -0,0 +1,124 @@
+package services
+
+import (
+	"backnedTestGolang/internal/dto"
+	"backnedTestGolang/internal/models"
+	"backnedTestGolang/internal/repository"
+	"errors"
+	"fmt"
+	"gorm.io/gorm"
+)
+
+type CartService interface {
+	AddProduct(cartID, productID uint64, quantity int) error
+	ReduceProduct(cartID, productID uint64, quantity int) error
+	RemoveProduct(cartID, ProductID uint64) error
+	GetCartItems(userID uint64) (*[]dto.ItemDTO, error)
+	Checkout(userID uint64) error
+}
+
+type cartServiceImpl struct {
+	cartRepo  repository.CartRepos
+	orderRepo repository.OrderRepos
+}
+
+func NewCartService(cartRepo repository.CartRepos, orderRepo repository.OrderRepos) CartService {
+	return &cartServiceImpl{cartRepo: cartRepo, orderRepo: orderRepo}
+}
+
+func (s *cartServiceImpl) AddProduct(cartID, productID uint64, quantity int) error {
+	const op = "cartServiceImpl.AddProduct"
+	var item *models.CartItem
+	var err error
+	if item, err = s.cartRepo.CheckItem(cartID, productID); err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	if item == nil {
+		if err = s.cartRepo.CreateItem(&models.CartItem{CartID: cartID, ProductID: productID}); err != nil {
+			return fmt.Errorf("%s: %w", op, err)
+		}
+		return nil
+	}
+	item.Quantity += quantity
+
+	if err = s.cartRepo.UpdateItem(item); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
+
+func (s *cartServiceImpl) ReduceProduct(cartID, productID uint64, quantity int) error {
+	const op = "cartServiceImpl.ReduceProduct"
+
+	var item *models.CartItem
+	var err error
+	if item, err = s.cartRepo.CheckItem(cartID, productID); err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	if item == nil {
+		return nil
+	} else if item.Quantity-quantity <= 0 {
+		if err = s.cartRepo.RemoveItem(cartID, productID); err != nil {
+			return fmt.Errorf("%s: %w", op, err)
+		}
+		return nil
+	}
+	item.Quantity -= quantity
+	if err = s.cartRepo.UpdateItem(item); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
+
+func (s *cartServiceImpl) RemoveProduct(cartID, ProductID uint64) error {
+	const op = "cartServiceImpl.RemoveProduct"
+
+	if err := s.cartRepo.RemoveItem(cartID, ProductID); err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
+
+func (s *cartServiceImpl) GetCartItems(userID uint64) (*[]dto.ItemDTO, error) {
+	const op = "cartServiceImpl.GetCartItems"
+
+	var items *[]dto.ItemDTO
+	var err error
+	if items, err = s.cartRepo.GetCartItems(userID); errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, err
+	} else if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return items, nil
+}
+
+func (s *cartServiceImpl) Checkout(userID uint64) error {
+	const op = "cartServiceImpl.Checkout"
+
+	cart, err := s.cartRepo.GetCart(userID)
+
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return err
+	} else if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	if cart.Items == nil {
+		return nil
+	}
+
+	orderItems := make([]models.OrderItem, len(cart.Items))
+	for i, item := range cart.Items {
+		orderItems[i] = models.OrderItem{ProductID: item.ProductID, Quantity: item.Quantity}
+	}
+
+	_, err = s.orderRepo.CreateOrder(&models.Order{UserID: cart.UserID, Items: orderItems})
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return err
+	} else if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	return nil
+}
