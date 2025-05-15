83f7f26b2508a9408519192bba1f34dc7ae56bb5
Frogpop <frogpop.forg@gmail.com>
2025-05-15 20:34:59 +0700

    add order handler

---
+++ internal/delivery/http/OrderHandler.go
@@ -0,0 +1,64 @@
+package http
+
+import (
+	"backnedTestGolang/internal/dto"
+	"backnedTestGolang/internal/services"
+	"backnedTestGolang/pkg/logger"
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+type OrderHandler interface {
+	GetUserOrders(c *gin.Context)
+	ChangeOrderStatus(c *gin.Context)
+}
+
+type orderHandler struct {
+	orderService services.OrderService
+}
+
+func NewOrderHandler(orderService services.OrderService) OrderHandler {
+	return &orderHandler{orderService: orderService}
+}
+
+func (h *orderHandler) GetUserOrders(c *gin.Context) {
+	var req dto.GetOrdersRequest
+
+	if err := c.ShouldBindQuery(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input", "details": logger.ValidationErrors(err).Error()})
+		return
+	}
+
+	orders, err := h.orderService.GetOrders(req.UserID)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		c.JSON(http.StatusNotFound, gin.H{"error": "record not found"})
+		return
+	} else if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, orders)
+}
+
+func (h *orderHandler) ChangeOrderStatus(c *gin.Context) {
+	var req dto.ChangeOrderStatusRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input", "details": logger.ValidationErrors(err).Error()})
+		return
+	}
+
+	if err := h.orderService.ChangeOrderStatus(req.OrderID, req.Status); errors.Is(err, gorm.ErrRecordNotFound) {
+		c.Error(fmt.Errorf("record not found"))
+		c.JSON(http.StatusNotFound, gin.H{"error": "record not found"})
+		return
+	} else if err != nil {
+		c.Error(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	c.Status(http.StatusOK)
+}
